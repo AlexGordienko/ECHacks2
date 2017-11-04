9d04d3ce197c5d5e072f9ab31589bffe9cfbbadc
sidg1999 <sid1999@live.ca>
2017-11-04 03:41:19 -0400

    Implemented parse_frames

--- backend/server/video.py
+++ backend/server/video.py
@@ -1,5 +1,8 @@
 from pytube import YouTube
-
+import cv2
+import time
+from typing import List
+from backend.server.frame import Frame
 
 class Video:
     """ Represents a uploaded youtube video
@@ -11,12 +14,16 @@ class Video:
     link: str
     directory: str
     frames: ['Frame']
+    name: str
+    fps: int
+    time: int
 
-    def __init__(self, link: str) -> None:
+    def __init__(self, link: str, name: str) -> None:
         """Initializes a video object"""
         self.link = link
         self.directory = None
         self.frames = []
+        self.name = name
 
     def download(self) -> None:
         """Downloads a youtube video to the drive"""
@@ -25,6 +32,28 @@ class Video:
         self.directory = './' + stream.default_filename
 
     def parse_frames(self) -> None:
-        """Computes the frames of the video"""
-        # TODO: parse the frames
-        pass
+        """
+        saves every tenth frame as a jpeg file in the 'server' directory,
+        and parses this video into a list of Frame objects
+
+        """
+
+        # openCV code used to read the frames in the video
+        vidcap = cv2.VideoCapture(self.directory)
+        success, image = vidcap.read()
+        current_frame = 0
+        while success:
+            success, image = vidcap.read()
+            current_frame += 1
+            # go through every tenth frame
+            if current_frame % 10 == 0:
+
+                # save frame as JPEG file
+                cv2.imwrite(self.name + "frame%d.jpg" % current_frame, image)
+
+                # get the name of this current frame
+                name = self.name + "frame%d.jpg"
+                time = current_frame * self.fps
+                new_frame = Frame(name, time)
+                new_frame.get_ocr_prediction()
+                self.frames.append(new_frame)
